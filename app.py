d65475a62b103f638bc8b0619a73b2c2c2967ba2
reneegt <reneegiront@gmail.com>
2026-03-10 15:07:39 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,48 @@
+import plotly.graph_objects as go  # Importación de plotly.graph_objects como go
+import pandas as pd
+import plotly.graph_objects as go  # Importación de plotly.graph_objects como go
+import streamlit as st
+
+st.header('Análisis Exploratorio de Datos: Mercado de Vehículos Usados')
+
+st.write('Este proyecto es una aplicación web interactiva para visualizar y analizar un conjunto de datos de anuncios de venta de coches en EUA')
+# Leer los datos del archivo CSV
+car_data = pd.read_csv('vehicles_us.csv')
+# Crear un botón en la aplicacion Streamlit
+hist_button = st.button('Construir histograma')
+
+# Lógica a ejecutar cuando se hace clic en el botón
+if hist_button:
+    # Escribir un mensaje en la aplicacion
+    st.write(
+        'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+
+    # Crear un histograma utilizando plotly.graph_objects
+    # Se crea una figura vacía y luego se añade un rastro de histograma
+    fig = go.Figure(data=[go.Histogram(x=car_data['odometer'])])
+
+    # Opcional: Puedes añadir un título al gráfico si lo deseas
+    fig.update_layout(title_text='Distribución del Odómetro')
+
+    # Mostrar el gráfico Plotly interactivo en la aplicación Streamlit
+    # 'use_container_width=True' ajusta el ancho del gráfico al contenedor
+    st.plotly_chart(fig, use_container_width=True)
+
+
+# Crear un scatter plot utilizando plotly.graph_objects
+# Se crea una figura vacía y luego se añade un rastro de scatter
+fig = go.Figure(data=[go.Scatter(x=car_data['odometer'],
+                y=car_data['price'], mode='markers')])
+
+# Opcional: Puedes añadir un título al gráfico si lo deseas
+fig.update_layout(title_text='Relación entre Odómetro y Precio')
+
+# Mostrar el gráfico Plotly
+fig.show()
+
+
+# crear una casilla de verificación
+build_histogram = st.checkbox('Construir un histograma')
+
+if build_histogram:  # si la casilla de verificación está seleccionada
+    st.write('Construir un histograma para la columna odómetro')
